775a9e332cce8c2aea4f82bee48c8ff6080de431
Michael Fischer <zarkal@live.com>
2019-06-09 17:02:24 -0700

    Added reset button and other changes

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -3,18 +3,29 @@ import ReactDOM from 'react-dom';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" onClick={props.onClick} style={props.winSq? {background: '#ffaaaa'} : {background: 'white'}}>
       {props.value}
     </button>
   );
 }
 
 class Board extends React.Component {
+  renderRow(i) {
+    return (
+      <div className="board-row">
+        {this.renderSquare(3 * i)}
+        {this.renderSquare(3 * i + 1)}
+        {this.renderSquare(3 * i + 2)}
+      </div>
+    )
+  }
+
   renderSquare(i) {
     return (
       <Square
         value={this.props.squares[i]}
         onClick={() => this.props.onClick(i)}
+        winSq={(this.props.winLine && (this.props.winLine[1] == i || this.props.winLine[2] == i || this.props.winLine[3] == i))? true : false}
       />
     );
   }
@@ -22,21 +33,9 @@ class Board extends React.Component {
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {this.renderRow(0)}
+        {this.renderRow(1)}
+        {this.renderRow(2)}
       </div>
     );
   }
@@ -48,6 +47,7 @@ class Game extends React.Component {
     this.state = {
       history: [{
         squares: Array(9).fill(null),
+        play: null,
       }],
       stepNumber: 0,
       xIsNext: true,
@@ -58,13 +58,14 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares) || squares[i] || this.state.stepNumber != this.state.history.length - 1) {
       return;
     }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{
         squares: squares,
+        play: [i, this.state.xIsNext ? 'X' : 'O'],
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
@@ -78,25 +79,38 @@ class Game extends React.Component {
     });
   }
 
+  reset() {
+    this.setState({
+      history: [{
+        squares: Array(9).fill(null),
+        play: null,
+      }],
+      stepNumber: 0,
+      xIsNext: true,
+    })
+  }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
+      let cur = false;
+      if (this.state.stepNumber == move) cur = true;
       const desc = move ?
-        'Move #' + move :
+        `${step.play[1]} took (${step.play[0] % 3 + 1},${(step.play[0] - (step.play[0] % 3)) / 3 + 1})` :
         'Game start';
       return (
         <li key={move}>
-          <a href="#" onClick={() => this.jumpTo(move)}>{desc}</a>
+          <a href="#" onClick={() => this.jumpTo(move)}>{cur ? <b>{desc}</b> : desc}</a>
         </li>
       );
     });
 
     let status;
     if (winner) {
-      status = 'Winner: ' + winner;
+      status = 'Winner: ' + winner[0];
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -107,10 +121,12 @@ class Game extends React.Component {
           <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
+            winLine={winner}
           />
         </div>
         <div className="game-info">
           <div>{status}</div>
+          <button onClick={() => this.reset()}>Reset</button>
           <ol>{moves}</ol>
         </div>
       </div>
@@ -132,7 +148,7 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a],a,b,c];
     }
   }
   return null;
